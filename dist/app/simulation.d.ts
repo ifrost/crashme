359cc78c7a9a56b6cc7a7c922d65e81f94c670d3
Piotr Jamróz <pm.jamroz@gmail.com>
2024-10-30 11:28:48 +0100

    Update distro

--- dist/app/simulation.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * A set of functions that can be used to simulate expensive operations that may crash of freeze the browser
- */
-export declare const crashmeSimulation: {
-    memoryCrash: () => void;
-    loopCrash: () => never;
-    recursiveCrash: () => void;
-    domCrash: () => never;
-};
--- dist/utils.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function getDb(dbName: string): Promise<unknown>;

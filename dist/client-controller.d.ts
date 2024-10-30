2ccf2389a3a9eebdbd48d9b06d8843a5e24f5906
Piotr Jamróz <pm.jamroz@gmail.com>
2024-10-30 22:59:21 +0100

    0.0.8

--- dist/client-controller.d.ts
+++ dist/client-controller.d.ts
@@ -1,9 +1,11 @@
 type CrashDetectionOptions<CustomProperties> = {
     id: string;
+    dbName: string;
     reportCrash: (crashedTab: CustomProperties) => Promise<boolean>;
     updateInfo: (currentTab: CustomProperties) => void;
     createDetectorWorker: () => SharedWorker;
     createClientWorker: () => Worker;
+    log?: (log: Record<string, string | boolean | number>) => void;
 };
 export type BasicReport = {
     id: string;
